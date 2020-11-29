16c9292a47b2e8264caf133e48dec954a7cf3aa9
sabbir-ahmmed-osmani <71857782+sabbir-ahmmed-osmani@users.noreply.github.com>
2020-11-29 10:58:49 +0600

    Create home.php

---
+++ Mobile_Banking_Task/home.php
@@ -0,0 +1,26 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+  
+
+<form method="POST" action="#">
+    <input type="text" name="amount" >
+    <input type="submit" name="submit" value="Deposit">
+</form>
+
+<?php
+        if(isset($_POST['submit'])){
+            $amount = $_POST['amount'];           
+            
+        }
+    ?>
+
+</body>
+</html>
