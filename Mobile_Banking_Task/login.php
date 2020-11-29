65eccba2b2690dbe22263d6ece9a8133bc9594db
sabbir-ahmmed-osmani <71857782+sabbir-ahmmed-osmani@users.noreply.github.com>
2020-11-29 10:59:28 +0600

    Create login.php

---
+++ Mobile_Banking_Task/login.php
@@ -0,0 +1,36 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+     
+    <form method="POST" action="#" >
+        <input type="text" name="name" >  
+        <input type="text" name="password" p> 
+        <input type="submit" name="submit" value="Login">
+    </form>
+    
+    <?php
+        if(isset($_POST['submit'])){
+            $name = $_POST['name'];
+            $password= $_POST['password'];
+
+            $sql = "Select * from user where name = '$name' and password='$password'";
+            $result = mysqli_query($conn,$sql);
+            if(mysqli_num_rows($result)){
+               echo "login done";
+            
+            }
+            else
+            {
+                echo "Login failed!";
+            }
+        }
+    ?>
+</body>
+</html>
