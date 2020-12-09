c79276676d880ee031342ef77b45a76c3d4809bc
sabbir-ahmmed-osmani <71857782+sabbir-ahmmed-osmani@users.noreply.github.com>
2020-12-09 21:52:20 +0600

    Create cvlists.php

---
+++ Mid/MidProject/view/cvlists.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Find_Your_Job</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+  <div style="display:inline-block;">
+    <?php include 'header.php' ?>
+  </div>
+
+    <div>
+      <p align="left">Best way to get your desired employee.</p>
+    </div>  
+
+   
+   	<div style="display:left;">
+    <nav>
+      <ul>
+         <li><a href="home.php">Home</a></li>
+         <li><a href="view/myjobs.php">My Jobs</a></li>
+      </ul>
+    </nav>
+  </div>
+
+
+   <div>
+    <h2 align="Center">CV Lists</h2>
+    
+    	<p align="left">1. For Assistant Manager,</br> 
+        Sajedul Islam, Bsc in Cse, 2 years of experience, sajedulislam@gmail.com, 01917283187
+      </p>
+
+    	<p align="left">1. For Manager,</br> 
+        Sajedul Islam, Bsc in Cse, 2 years of experience, Nirob@gmail.com, 01387728739
+      </p>
+
+
+    	<p align="left">1. For It engineer,</br> 
+        Rahul Roy, Bsc in Cse, 2 years of experience, rahul roy@gmail.com, 01917283187</p>
+   </div> 	
+
+
+   
+   
+    
+
+   
+  <div>
+    <?php include 'footer.php' ?>
+  </div>
+</body>
+</html>
